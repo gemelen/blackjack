768c1d3e492cf868efff2bba3cd1784a4c4848d3
gemelen <pyshjev-ds@cher.metcombank.ru>
2014-12-09 11:02:01 +0300

    hz changes

--- src/main/java/net/gemelen/blackjack/data/DataGrid.java
+++ src/main/java/net/gemelen/blackjack/data/DataGrid.java
@@ -1,17 +1,33 @@
 package net.gemelen.blackjack.data;
 
-import com.hazelcast.config.Config;
+import com.hazelcast.config.*;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import java.util.Map;
 
 public class DataGrid {
+    private static final String SETTINGS_MAP = "casino";
+    private static final String PLAYERS_MAP = "casino";
+
     private Map<String, String> casino;
     private Map<Integer, PlayerRecordView> players;
 
     public DataGrid() {
         Config cfg = new Config();
+
+        MapConfig mc = new MapCOnfig();
+        mc.setName(SETTINGS_MAP);
+        mc.setBackupCount(1);
+        mc.setReadBackupData(true);
+        cfg.addMapConfig(mc);
+        MapConfig pc = new MapCOnfig();
+        pc.setName(PLAYERS_MAP);
+        pc.setBackupCount(1);
+        pc.setReadBackupData(true);
+        cfg.addMapConfig(pc);
+
         HazelcastInstance grid = Hazelcast.newHazelcastInstance(cfg);
+
         this.casino = grid.getMap("casino");
         this.players = grid.getMap("players");
     }
