4369029fc4cc8709171640d834cbf39e389ee80e
gemelen <gemelen@gmail.com>
2014-12-10 18:36:44 +0300

    now I know that it really started :|

--- src/main/java/net/gemelen/blackjack/TwentyOne.java
+++ src/main/java/net/gemelen/blackjack/TwentyOne.java
@@ -5,16 +5,13 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 import java.util.Hashtable;
-import java.util.logging.Logger;
 
 public class TwentyOne implements BundleActivator {
-  private final static Logger log = Logger.getAnonymousLogger();
-
   @Override
   public void start(BundleContext context) throws Exception {
     Hashtable<String, String> props = new Hashtable<String, String>();
     context.registerService(InstantiateDealer.class.getName(), new InstantiateDealerImpl(), props);
-    log.fine("started");
+    System.out.println("Blackjack: bundle started");
   }
 
   @Override
